0ed6af0281f4330226578ad1e0568e0cb3e70486
D-Jack <siddharthauchiha50@gmail.com>
2021-01-16 14:39:36 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,69 @@
+let framedata;
+
+console.log(framedata);
+let frameNo = 0;
+const nextBtn = document.getElementById("next");
+const conBtn = document.getElementById("con");
+const prevBtn = document.getElementById("prev");
+const submitBtn = document.getElementById("submit");
+
+function setVideoUrl(frameNo) {
+  const activeVideo = document.getElementById("activeVideo");
+  activeVideo.innerHTML = 
+  `<video width="${videoWidth}" controls>
+    <source src="${videoUrl[frameNo]}" type="video/mp4">
+    Your browser does not support HTML video.
+  </video>`;
+}
+
+function editEntry(inputId) {
+  frameEntries[frameNo].frameId = `${inputId}`;
+}
+
+function nextEntry() {
+  console.log(frameNo);
+
+  if (frameNo >= 3) {
+    nextBtn.disabled = true;
+  }
+  frameNo++;
+  setVideoUrl(frameNo);
+  prevBtn.disabled = false;
+}
+function prevEntry() {
+  console.log(frameNo);
+  if (frameNo <= 1) {
+    prevBtn.disabled = true;
+  }
+  frameNo--;
+  setVideoUrl(frameNo);
+  nextBtn.disabled = false;
+}
+function con() {
+  console.log(frameEntries, frameNo);
+}
+
+let frameEntries = [];
+for (let i = 0; i < 5; i++) {
+  let frameEntry = {
+    id: `v${i + 1}`,
+    frameId: "1",
+  };
+  frameEntries.push(frameEntry);
+}
+
+$(document).ready(function () {
+  $('input[type="radio"]').click(function () {
+    let inputId = $(this).attr("id");
+    editEntry(inputId);
+  });
+});
+
+function submitHandler() {
+  handleSubmit();
+}
+
+nextBtn.addEventListener("click", nextEntry);
+prevBtn.addEventListener("click", prevEntry);
+conBtn.addEventListener("click", con);
+submitBtn.addEventListener("click", submitHandler);
---
+++ vendor.js
@@ -0,0 +1,56 @@
+//global variables
+const videoUrl = [];
+const videoWidth = 500;
+function fetchdata() {
+  fetch("http://www.mocky.io/v2/5ed5fda4340000740006d560?mocky-delay=500ms")
+    .then((Response) => {
+      if (!Response.ok) {
+        throw Error("ERROR");
+      }
+      return Response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      getVideoId(data);
+      setUrlBackground(data);
+      getVideoUrl(data);
+    });
+}
+function handleSubmit() {
+  fetch("http://www.mocky.io/v2/5ed609363400004d0006d602", {
+    method: "POST",
+    body: JSON.stringify(frameEntries),
+  }).then((data) => {
+    console.log(data);
+  });
+}
+
+function getVideoId(data) {
+  const arr = [];
+  for (const frameEl of data) {
+    arr.push(frameEl.id);
+  }
+}
+
+function getVideoUrl(data) {
+  for (const frameEl of data) {
+    videoUrl.push(frameEl.url);
+  }
+  const activeVideo = document.getElementById("activeVideo");
+  activeVideo.innerHTML = `<video width="${videoWidth}" controls>
+    <source src="${videoUrl[0]}" type="video/mp4">
+    Your browser does not support HTML video.
+  </video>`;
+}
+
+const setUrlBackground = (data) => {
+  let i = 1;
+  for (const frameEl of data) {
+    const label = document.querySelector(`.frame${i}`);
+    // console.log(label);
+    label.style.backgroundImage = `url('${frameEl.frames[0].url}')`;
+    i++;
+  }
+};
+
+fetchdata();
